bed68c7e33cb4ac254125b4de95954df649a53d6
Dylan Dalal <dylanmax@gmail.com>
2024-10-14 15:28:55 -0400

    modal updates

--- src/resources/Modal.js
+++ src/resources/Modal.js
@@ -1,24 +1,33 @@
 import React, { useEffect } from 'react';
 import './Modal.css';
+import '../pages/Tech.css';
 
-
-
-const Modal = ({ isOpen, content, onClose }) => {
+const Modal = ({ isOpen, headerText, bodyContent, onClose }) => {
   useEffect(() => {
     if (isOpen) {
-      document.body.style.overflow = 'hidden';  // Disable scrolling
+      document.body.style.overflow = 'hidden'; // Disable background scrolling
     } else {
-      document.body.style.overflow = 'auto';    // Re-enable scrolling
+      document.body.style.overflow = 'auto'; // Enable background scrolling
     }
   }, [isOpen]);
 
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay">
+    <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <span className="close-button" onClick={onClose}>&times;</span>
-        <div>{content}</div>
+        <div className="modal-inner">
+          <span className="close-button" onClick={onClose}>&times;</span>
+          <div className="modal-header" style={{ textAlign: 'center' }}>
+            <h className="modal-header-text">{headerText}</h>
+            <div className="subheader">
+              PIPELINE DEVELOPMENT
+            </div>
+          </div>
+          <div className="modal-body">
+            {bodyContent}
+          </div>
+        </div>
       </div>
     </div>
   );
