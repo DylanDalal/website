9b1a41b2e429e84a2f35eae076b66692b52e1134
Dylan Dalal <dylanmax@gmail.com>
2024-11-06 14:40:55 -0500

    add footer interaction wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,28 @@
-import React, { useRef, useState, useEffect } from 'react';
-import Radium, { StyleRoot } from 'radium';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
 function Footer({ onSelectPage }) {
-
     return (
-        <StyleRoot>
-            <div style={{backgroundColor: "#000630",
-                         height: "15vh",
-                         width: "100%",
-                         display: "flex",
-                         fontFamily: "Futura",
-                         justifyContent: "center",
-                         fontSize: "2vh"
-                         }}>
-                <div style={{ display: "flex", alignItems: "center", gap: "3vw"}}>
-                    <div className="footer-text">Film</div>
-                    <div className="footer-text">Home</div>
-                    <div className="footer-text">Tech</div>
-                </div>
-                <div style={{ display: "flex", alignItems: "center", margin: "0 0 0 10vw"}}>
-                    <div className="footer-text">Contact</div>
-                </div>
+        <div style={{
+            backgroundColor: "#000630",
+            height: "15vh",
+            width: "100%",
+            display: "flex",
+            fontFamily: "Futura",
+            justifyContent: "center",
+            fontSize: "2vh",
+        }}>
+            <div style={{ display: "flex", alignItems: "center", gap: "3vw" }}>
+                <Link to="/film" className="footer-text" onClick={() => onSelectPage('Film')}>Film</Link>
+                <Link to="/home" className="footer-text" onClick={() => onSelectPage('Home')}>Home</Link>
+                <Link to="/tech" className="footer-text" onClick={() => onSelectPage('Tech')}>Tech</Link>
             </div>
-        </StyleRoot>
+            <div style={{ display: "flex", alignItems: "center", margin: "0 0 0 10vw" }}>
+                <Link to="/contact" className="footer-text" onClick={() => onSelectPage('Contact')}>Contact</Link>
+            </div>
+        </div>
     );
 }
 
-
-export default Radium(Footer);
+export default Footer;
