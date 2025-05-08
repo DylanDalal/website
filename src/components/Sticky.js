8b413bb67b5b96e763db52c384c2b175f440abcb
Dylan Dalal <dylanmax@gmail.com>
2025-05-08 00:08:44 -0400

    change sticky functionality

--- src/components/Sticky.js
+++ src/components/Sticky.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./StickyNavbar.scss";
 
 export default function StickyNavbar() {
   const [show, setShow] = useState(false);
+  const location = useLocation();
+
+  const isWebRoute = location.pathname === "/web";
 
   useEffect(() => {
+    if (isWebRoute) {
+      setShow(true); // Always show for /web
+      return;        // Skip scroll listener
+    }
+
     const onScroll = () =>
       setShow(window.scrollY >= window.innerHeight * 2.25);
 
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
-  }, []);
+  }, [isWebRoute]);
 
   return (
     <div className={`stickyNavbar ${show ? "show" : "hide"}`}>
